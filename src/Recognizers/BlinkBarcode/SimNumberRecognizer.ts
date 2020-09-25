647974b87d52f077c6bc77eba8fbb7cfd2ef7080
Jenkins <jenkins@rambo>
2020-09-25 14:42:04 +0000

    Automatically built release by Jenkins

---
+++ src/Recognizers/BlinkBarcode/SimNumberRecognizer.ts
@@ -0,0 +1,42 @@
+import { Recognizer, RecognizerResult, RecognizerSettings, WasmSDK } from "../../MicroblinkSDK/DataStructures";
+
+/**
+ * A settings object that is used for configuring the SimNumberRecognizer.
+ */
+export class SimNumberRecognizerSettings implements RecognizerSettings
+{}
+
+/**
+ * The result of image recognition when using the SimNumberRecognizer.
+ */
+export interface SimNumberRecognizerResult extends RecognizerResult
+{
+    /**
+     *  The sim number
+     */
+    readonly simNumber: string;
+}
+
+/**
+ * The Sim Number Recognizer is used for scanning Sim Number.
+ */
+export interface SimNumberRecognizer extends Recognizer
+{
+    /** Returns the currently applied SimNumberRecognizerSettings. */
+    currentSettings(): Promise< SimNumberRecognizerSettings >
+
+    /** Applies new settings to the recognizer. */
+    updateSettings( newSettings: SimNumberRecognizerSettings ): Promise< void >;
+
+    /** Returns the current result of the recognition. */
+    getResult(): Promise< SimNumberRecognizerResult >;
+}
+
+/**
+ * This function is used to create a new instance of `SimNumberRecognizer`.
+ * @param wasmSDK Instance of WasmSDK which will be used to communicate with the WebAssembly module.
+ */
+export async function createSimNumberRecognizer( wasmSDK: WasmSDK ): Promise< SimNumberRecognizer >
+{
+    return wasmSDK.mbWasmModule.newRecognizer( "SimNumberRecognizer" ) as Promise< SimNumberRecognizer >;
+}
